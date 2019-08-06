e8c1ec54d9b3415d35e91bd9a68eb9ce474a640a
Douglas Goodall <doug@goodall.com>
2019-08-06 07:30:27 -0700

    with returnPi function

--- Makefile
+++ Makefile
@@ -4,3 +4,6 @@
 hello:  hello.cpp
 	g++ hello.cpp -o hello -pthread -lgtest_main -lgtest
 
+clean:
+	rm -f hello
+
--- hello.cpp
+++ hello.cpp
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 #include <gtest/gtest.h>
 
+double returnPi() {
+	return (double)3.14;
+}
+
 int testmain()
 {
-  printf("Hello World Five 😀!!\n");
-  return EXIT_SUCCESS;
+	printf("Hello World Five 😀!!\n");
+	printf("pi is %2.4f\n",returnPi());
+	return EXIT_SUCCESS;
 }
 
 TEST(MyTestSuite,first_test) {
 	EXPECT_EQ(0,testmain());
+}
+
+TEST(MyTestSuite,test_returnPi) {
+	EXPECT_EQ(3.14,returnPi());
 }
